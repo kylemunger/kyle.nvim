9dcde95c2d7e34ffbd8e702a0a87df72c4594f30
Kyle Unger <kylemunger99@gmail.com>
2024-01-12 03:41:51 +0000

    harpoon

--- lua/core/plugin_config/harpoon_config.lua
+++ lua/core/plugin_config/harpoon_config.lua
@@ -3,3 +3,39 @@ vim.keymap.set('n', '<leader>Hm', ':lua require("harpoon.mark").add_file()<CR>',
 vim.keymap.set('n', '<leader>Hl', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>Hn', ':lua require("harpoon.ui").nav_next()<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>Hp', ':lua require("harpoon.ui").nav_prev()<CR>', { noremap = true, silent = true })
+
+local function run_python_in_tmux_right()
+    -- Save the current file
+    vim.cmd(':w')
+
+    -- Get the current file name
+    local file_name = vim.fn.expand('%')
+
+    -- Send the python3 run command to the tmux pane on the right
+    require('harpoon.tmux').sendCommand('{right-of}', 'python3 ' .. file_name)
+
+    -- Navigate to the tmux pane on the right
+    require('harpoon.tmux').gotoTerminal('{right-of}')
+end
+
+local function run_python_with_args_in_tmux_right()
+    -- Save the current file
+    vim.cmd(':w')
+
+    -- Get the current file name
+    local file_name = vim.fn.expand('%')
+
+    -- Prompt for custom arguments
+    local args = vim.fn.input('Enter Python arguments: ')
+
+    -- Send the python3 run command with custom arguments to the tmux pane on the right
+    require('harpoon.tmux').sendCommand('{right-of}', 'python3 ' .. file_name .. ' ' .. args)
+
+    -- Navigate to the tmux pane on the right
+    require('harpoon.tmux').gotoTerminal('{right-of}')
+end
+
+vim.api.nvim_create_user_command('RunPythonTmuxRight', run_python_in_tmux_right, {})
+vim.api.nvim_create_user_command('RunPythonTmuxRightWithArgs', run_python_with_args_in_tmux_right, {})
+vim.keymap.set('n', '<leader>rt', ':RunPythonTmuxRight<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ra', ':RunPythonTmuxRightWithArgs<CR>', { noremap = true, silent = true })
